f5a174f929484b62b7d94e8907ed42ad7f848575
Marc Joliet <marcec@gmx.de>
2013-04-24 13:40:10 +0200

    Remove dependency on separate extra_types.h.
    Remove the dependency on a separate extra_types.h file from the cffi wrapper and
    instead just put the code inside the ffi.cdef() string.
    
    Also, switch the string parameterization from str.format() to
    Template.substitute() in order to a) keep the string definition simple (avoid
    string concatenation) and b) keep the inline C code close to C (avoid "{{" and
    "}}" escapes due to format()).

--- wrapper.py
+++ wrapper.py
@@ -1,4 +1,5 @@
 import cffi
+from string import Template
 
 # define the ffi object
 ffi = cffi.FFI()
@@ -54,11 +55,44 @@ ffi.cdef(cdefs)
 
 # compile the code
 C = ffi.verify(
-    """
-    #define FAUSTFLOAT {0}
-    #include "extra_types.h"
-    #include "{1}.h"
-    """.format(FAUSTFLOAT, FAUSTDSP),
+    Template("""
+    #define FAUSTFLOAT ${FAUSTFLOAT}
+
+    // helper function definitions
+    int min(int x, int y) { return x < y ? x : y;};
+    int max(int x, int y) { return x > y ? x : y;};
+
+    // the MetaGlue struct that will be wrapped
+    typedef struct {
+        void *mInterface;
+        void (*declare)(void* interface, const char* key, const char* value);
+    } MetaGlue;
+
+    // the UIGlue struct that will be wrapped
+    typedef struct {
+        // widget layouts
+        void (*openVerticalBox)(void*, const char* label);
+        void (*openHorizontalBox)(void*, const char* label);
+        void (*openTabBox)(void*, const char* label);
+        void (*declare)(void*, FAUSTFLOAT*, char*, char*);
+        // passive widgets
+        void (*addNumDisplay)(void*, const char* label, FAUSTFLOAT* zone, int p);
+        void (*addTextDisplay)(void*, const char* label, FAUSTFLOAT* zone, const char* names[], FAUSTFLOAT min, FAUSTFLOAT max);
+        void (*addHorizontalBargraph)(void*, const char* label, FAUSTFLOAT* zone, FAUSTFLOAT min, FAUSTFLOAT max);
+        void (*addVerticalBargraph)(void*, const char* label, FAUSTFLOAT* zone, FAUSTFLOAT min, FAUSTFLOAT max);
+        // active widgets
+        void (*addHorizontalSlider)(void*, const char* label, FAUSTFLOAT* zone, FAUSTFLOAT init, FAUSTFLOAT min, FAUSTFLOAT max, FAUSTFLOAT step);
+        void (*addVerticalSlider)(void*, const char* label, FAUSTFLOAT* zone, FAUSTFLOAT init, FAUSTFLOAT min, FAUSTFLOAT max, FAUSTFLOAT step);
+        void (*addButton)(void*, const char* label, FAUSTFLOAT* zone);
+        void (*addToggleButton)(void*, const char* label, FAUSTFLOAT* zone);
+        void (*addCheckButton)(void*, const char* label, FAUSTFLOAT* zone);
+        void (*addNumEntry)(void*, const char* label, FAUSTFLOAT* zone, FAUSTFLOAT init, FAUSTFLOAT min, FAUSTFLOAT max, FAUSTFLOAT step);
+        void (*closeBox)(void*);
+        void* uiInterface;
+    } UIGlue;
+
+    #include "${FAUSTDSP}.h"
+    """).substitute(FAUSTFLOAT=FAUSTFLOAT, FAUSTDSP=FAUSTDSP),
     libraries=[],
     include_dirs=["."],
     extra_compile_args=["-std=c99"],
