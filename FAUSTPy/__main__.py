73da3d9a821bcace2e350e23c8f4585dde292b98
Marc Joliet <marcec@gmx.de>
2013-05-01 13:35:53 +0200

    Convert CData objects to float before comparison.
    Convert CData objects to float before comparison to handle the case of
    FAUSTFLOAT == "long double", since dereferencing CData objects (i.e., "obj[0]")
    does not work in that case (perhaps because there is no long double literal in
    Python).

--- FAUSTPy/__main__.py
+++ FAUSTPy/__main__.py
@@ -38,12 +38,12 @@ ui = PythonUI(ffi, bla).ui
 ui.openVerticalBox(ffi.NULL,"bla")
 
 slider_val = ffi.new("FAUSTFLOAT*", 1.0)
-assert slider_val[0] == 1.0
+assert float(slider_val[0]) == 1.0
 ui.addHorizontalSlider(ffi.NULL, "float", slider_val, 0.0, 0.0, 2.0, 0.1)
 assert hasattr(bla.bla, "float")
-assert bla.bla.float.zone == 0.0
+assert float(bla.bla.float.zone) == 0.0
 bla.bla.float.zone = 0.5
-assert bla.bla.float.zone == slider_val[0]
+assert float(bla.bla.float.zone) == float(slider_val[0])
 
 button_val = ffi.new("FAUSTFLOAT*", 1.0)
 # should do nothing
