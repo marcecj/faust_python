49365ee0823e03abaf46727c7c0bd13d59f14d30
Marc Joliet <marcec@gmx.de>
2013-05-05 15:35:20 +0200

    __main__.py: add plots with varying parameters.
    Add plots that display the magnitude frequency response with varying parameters.

--- FAUSTPy/__main__.py
+++ FAUSTPy/__main__.py
@@ -69,6 +69,101 @@ p   = fig.add_subplot(
 p.plot(20*np.log10(np.absolute(spec.T)+1e-8))
 p.legend(("Left channel", "Right channel"), loc="best")
 
+#######################################################
+# plot the frequency response with varying Q
+#######################################################
+
+Q  = np.linspace(def_Q.min, def_Q.max, 10)
+
+dattorro.dsp.dattorro_notch_cut_regalia.Center_Freq_ = 1e2
+dattorro.dsp.dattorro_notch_cut_regalia.Gain = 10**(-0.5) # -10 dB
+
+cur_G = dattorro.dsp.dattorro_notch_cut_regalia.Gain.zone
+cur_F = dattorro.dsp.dattorro_notch_cut_regalia.Center_Freq_.zone
+
+fig = plt.figure()
+p = fig.add_subplot(
+    1,1,1,
+    title="Frequency response "
+          "(G={:.0f} dB FS, F={} Hz)".format(20*np.log10(cur_G+1e-8), cur_F),
+    xlabel="Frequency in Hz (log)",
+    ylabel="Magnitude in dB FS",
+    xscale="log"
+)
+
+for q in Q:
+    dattorro.dsp.dattorro_notch_cut_regalia.Q = q
+    out = dattorro.compute(audio)
+    spec = np.fft.fft(out)[0,:args.fs/2]
+
+    p.plot(20*np.log10(np.absolute(spec.T)+1e-8),
+           label="Q={}".format(q))
+
+p.legend(loc="best")
+
+#######################################################
+# plot the frequency response with varying gain
+#######################################################
+
+# start at -60 dB because the minimum is at an extremely low -160 dB
+G  = np.logspace(-3, np.log10(def_Gain.max), 10)
+
+dattorro.dsp.dattorro_notch_cut_regalia.Q = 2
+
+cur_Q = dattorro.dsp.dattorro_notch_cut_regalia.Q.zone
+cur_F = dattorro.dsp.dattorro_notch_cut_regalia.Center_Freq_.zone
+
+fig = plt.figure()
+p = fig.add_subplot(
+    1,1,1,
+    title="Frequency response (Q={}, F={} Hz)".format(cur_Q, cur_F),
+    xlabel="Frequency in Hz (log)",
+    ylabel="Magnitude in dB FS",
+    xscale="log"
+)
+
+for g in G:
+    dattorro.dsp.dattorro_notch_cut_regalia.Gain = g
+    out = dattorro.compute(audio)
+    spec = np.fft.fft(out)[0,:args.fs/2]
+
+    p.plot(20*np.log10(np.absolute(spec.T)+1e-8),
+           label="G={:.3g} dB FS".format(20*np.log10(g+1e-8)))
+
+p.legend(loc="best")
+
+###########################################################
+# plot the frequency response with varying center frequency
+###########################################################
+
+F  = np.logspace(np.log10(def_Freq.min), np.log10(def_Freq.max), 10)
+
+dattorro.dsp.dattorro_notch_cut_regalia.Q    = def_Q.default
+dattorro.dsp.dattorro_notch_cut_regalia.Gain = 10**(-0.5) # -10 dB
+
+cur_Q = dattorro.dsp.dattorro_notch_cut_regalia.Q.zone
+cur_G = dattorro.dsp.dattorro_notch_cut_regalia.Gain.zone
+
+fig = plt.figure()
+p = fig.add_subplot(
+    1,1,1,
+    title="Frequency response "
+          "(Q={}, G={:.0f} dB FS)".format(cur_Q, 20*np.log10(cur_G+1e-8)),
+    xlabel="Frequency in Hz (log)",
+    ylabel="Magnitude in dB FS",
+    xscale="log"
+)
+
+for f in F:
+    dattorro.dsp.dattorro_notch_cut_regalia.Center_Freq_ = f
+    out = dattorro.compute(audio)
+    spec = np.fft.fft(out)[0,:args.fs/2]
+
+    p.plot(20*np.log10(np.absolute(spec.T)+1e-8),
+           label="F={:.2f} Hz".format(f))
+
+p.legend(loc="best")
+
 ################
 # show the plots
 ################
