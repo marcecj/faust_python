3f949870592c9b6d41e4c4d0fc9ae779e301705f
Marc Joliet <marcec@gmx.de>
2013-05-06 00:13:43 +0200

    Remove python-2.{5,6} from setup.py "classifiers".
    Remove python-2.{5,6} from setup.py "classifiers" because of the recent changes
    that make the source code work unchanged on Python 2 and 3 (e.g., use of b""
    string literals).

--- setup.py
+++ setup.py
@@ -29,8 +29,6 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: POSIX :: Linux',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.5',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.2',
