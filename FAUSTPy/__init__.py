f517f44ea5140e71ede60cb6a3be3a97f4957430
Marc Joliet <marcec@gmx.de>
2013-05-12 22:45:35 +0200

    Import PythonMeta into the FAUSTPy namespace.
    Import PythonMeta into the FAUSTPy namespace and add it to FAUSTPy.__all__.
    Also add a reference to PythonMeta in the package documentation.

--- FAUSTPy/__init__.py
+++ FAUSTPy/__init__.py
@@ -5,6 +5,7 @@ A set of classes used to dynamically wrap FAUST DSP programs in Python.
 
 This package defines three types:
 - PythonUI is an implementation of the UIGlue C struct.
+- PythonMeta is an implementation of the MetaGlue C struct.
 - FAUSTDsp wraps the DSP struct.
 - FAUST integrates the other two, sets up the CFFI environment (defines the
   data types and API) and compiles the FAUST program.  This is the class you
@@ -13,6 +14,7 @@ This package defines three types:
 
 from . wrapper import FAUST
 from . python_ui import PythonUI, Param
+from . python_meta import PythonMeta
 from . python_dsp import FAUSTDsp
 
 # TODO: see which meta-data is still relevant. pydoc definitely uses "author",
@@ -26,4 +28,4 @@ __maintainer__       = "Marc Joliet"
 __email__            = "marcec@gmx.de"
 __status__           = "Prototype"
 
-__all__ = ["FAUST", "PythonUI", "FAUSTDsp", "Param", "wrapper"]
+__all__ = ["FAUST", "PythonUI", "PythonMeta", "FAUSTDsp", "Param", "wrapper"]
