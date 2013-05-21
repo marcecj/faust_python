7af224a7faf4ddfb2bea7d5fef747e096ad3215f
Marc Joliet <marcec@gmx.de>
2013-05-22 01:56:12 +0200

    Tests: fix definitions of min/max.
    I forgot to fix these when I fixed them in FAUSTPy.wrapper.

--- test/helpers.py
+++ test/helpers.py
@@ -77,8 +77,8 @@ def init_ffi(faust_dsp="dattorro_notch_cut_regalia.dsp",
             #define FAUSTFLOAT ${FAUSTFLOAT}
 
             // helper function definitions
-            int min(int x, int y) { return x < y ? x : y;};
-            int max(int x, int y) { return x > y ? x : y;};
+            FAUSTFLOAT min(FAUSTFLOAT x, FAUSTFLOAT y) { return x < y ? x : y;};
+            FAUSTFLOAT max(FAUSTFLOAT x, FAUSTFLOAT y) { return x > y ? x : y;};
 
             // the MetaGlue struct that will be wrapped
             typedef struct {
